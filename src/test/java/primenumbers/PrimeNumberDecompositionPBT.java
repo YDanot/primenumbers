de9758aab2f8b9b1146579f97b885f403651d3f9
Yann DANOT <yann@arolla.fr>
2018-07-12 15:32:48 +0200

    Add test for propertie : the multiplication of all decomposed factor should be equals to the decomposed value itself.

--- src/test/java/primenumbers/PrimeNumberDecompositionPBT.java
+++ src/test/java/primenumbers/PrimeNumberDecompositionPBT.java
@@ -1,4 +1,23 @@
 package primenumbers;
 
+import com.pholser.junit.quickcheck.Property;
+import com.pholser.junit.quickcheck.generator.InRange;
+import com.pholser.junit.quickcheck.runner.JUnitQuickcheck;
+import org.junit.runner.RunWith;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(JUnitQuickcheck.class)
 public class PrimeNumberDecompositionPBT {
+
+    private final PrimeNumberFactorDecomposition primeNumberFactorDecomposition = new PrimeNumberFactorDecomposition();
+
+    @Property
+    public void the_multiplication_of_all_factors_should_be_equals_to_input_value(@InRange(minInt = 1, maxInt = 1000000) int numericValue) {
+        Optional<Integer> multiplication = primeNumberFactorDecomposition.decompose(numericValue).stream().reduce((i1, i2) -> i1 * i2);
+        assertThat(multiplication).contains(numericValue);
+    }
+
 }
